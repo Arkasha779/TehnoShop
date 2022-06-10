db76624d5da1892c10edae15042306b285754fe2
parohodov@hotmail.com <parohodov@hotmail.com>
2022-06-10 11:47:13 +0300

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TehnoShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
